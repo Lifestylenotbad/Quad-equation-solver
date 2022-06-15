9cc48349445fd68709d7a71d517f4ba37d56bfc6
Lifestylenotbad <107487122+Lifestylenotbad@users.noreply.github.com>
2022-06-15 20:20:26 +0300

    Update quadsolver.c

--- quadsolver.c
+++ quadsolver.c
@@ -3,10 +3,9 @@
 
 int main()
 {
-	int   d;
-	float a, b, c, x1, x2, x;	
+	float a, b, c, d, x1, x2, x;	
 	printf("To find the root of your equation, enter the coefficients a, b and c: \n");
-	scanf("%f", &a); scanf("%f", &b); scanf("%f", &c);
+	scanf("%f %f %f", &a, &b, &c);
 	d = b * b - 4 * a * c;
         if(d > 0) {
 		x1 = (-b + sqrt(d)) / (2 * a);
@@ -20,6 +19,6 @@ int main()
 	if(d < 0) { 
 		printf("There are no roots");	
 	}
-getchar();
-return 0;
+        getchar();
+        return 0;
 }
